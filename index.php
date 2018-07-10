b01f7d05c0daa7532376311552ee06d41b203320
bte8444 <10855096+bte8444@users.noreply.github.com>
2018-07-10 15:24:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
    <h1>Hello world from Github in a PHP file!!</h1>
    <ul>
       <li>Slot message:  <?php echo getenv("slotMessage"); ?></li>
+      <li>Api key:  <?php echo getenv("apiKey"); ?></li>
+      <li>Default conn string:  <?php echo getenv("SQLAZURE_DefaultConnString"); ?></li>
+      <li>Slot conn string:  <?php echo getenv("SQLAZURE_SlotConnString"); ?></li>
    </ul>
 </body>
 </html>
