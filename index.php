d5869810802179fca8688ec0ef8a63b8670e1d86
bte8444 <10855096+bte8444@users.noreply.github.com>
2018-07-10 15:29:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
    <ul>
       <li>Slot message:  <?php echo getenv("slotMessage"); ?></li>
       <li>Api key:  <?php echo getenv("apiKey"); ?></li>
-      <li>Default conn string:  <?php echo getenv("SQLAzure_DefaultConnString"); ?></li>
-      <li>Slot conn string:  <?php echo getenv("SQLAzure_SlotConnString"); ?></li>
+      <li>Default conn string:  <?php echo getenv("SQLAZURECONNSTR_DefaultConnString"); ?></li>
+      <li>Slot conn string:  <?php echo getenv("SQLAZURECONNSTR_SlotConnString"); ?></li>
    </ul>
 </body>
 </html>
